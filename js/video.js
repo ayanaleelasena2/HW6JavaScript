01307342c5d1d52549b3be8cae896c7032b31fbe
Ayana Leelasena <ayanal@umich.edu>
2025-04-11 20:11:50 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,89 +1,106 @@
 var video;
 
+
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+   console.log("Good job opening the window")
+
 
 });
+
 
 //Page load
 var video;
 
+
 window.addEventListener("load", function() {
-    console.log("Good job opening the window");
-    video = document.querySelector('video');
-    video.autoplay = false;
-    video.loop = false;
+   console.log("Good job opening the window");
+   video = document.querySelector('video');
+   video.autoplay = false;
+   video.loop = false;
 });
+
 
 //Play Button
 document.querySelector("#play").addEventListener("click", function() {
-    console.log("Play Video");
-    video.play();
-    updateVolumeInfo();
+   console.log("Play Video");
+   video.play();
+   updateVolumeInfo();
 });
+
 
 //Pause Button
 document.querySelector("#pause").addEventListener("click", function() {
-    console.log("Pause Video");
-    video.pause();
+   console.log("Pause Video");
+   video.pause();
 });
+
 
 //Slow Down
 document.querySelector("#slower").addEventListener("click", function() {
-    video.playbackRate *= 0.7;
-    console.log("New speed is " + video.playbackRate);
+   video.playbackRate *= 0.9;
+   console.log("New speed is " + video.playbackRate);
 });
+
 
 //Speed Up
 document.querySelector("#faster").addEventListener("click", function() {
-    video.playbackRate /= 0.7;
-    console.log("New speed is " + video.playbackRate);
+   video.playbackRate /= 0.9;
+   console.log("New speed is " + video.playbackRate);
 });
+
 
 //Skip Ahead
 document.querySelector("#skip").addEventListener("click", function() {
-    if (video.currentTime + 10 > video.duration) {
-        video.currentTime = 0;
-    console.log("Current location is " + video.currentTime);
-    }
+   if (video.currentTime + 10 > video.duration) {
+       video.currentTime = 0;
+   } else {
+       video.currentTime += 10;
+   }
+   console.log("Current location is " + video.currentTime);
 });
+
 
 //Mute
 document.querySelector("#mute").addEventListener("click", function() {
-    if (video) {
-        video = false;
-        this.textContent = "Mute";
-		console.log("Unmute")
-    } else {
-        video = true;
-        this.textContent = "Unmute";
-		console.log("Mute")
-    }
+   if (video.muted) {
+       video.muted = false;
+       this.textContent = "Mute";
+       console.log("Unmute")
+   } else {
+       video.muted = true;
+       this.textContent = "Unmute";
+       console.log("Mute")
+   }
 });
+
 
 //Volume Slider
 document.querySelector("#slider").addEventListener("input", function() {
-    video.volume = this.value / 100;
-    updateVolumeInfo();
-    console.log("Volume is " + video.volume);
+   video.volume = this.value / 100;
+   updateVolumeInfo();
+   console.log("Volume is " + video.volume);
 });
 
+
 function updateVolumeInfo() {
-    var volumeDisplay = document.querySelector('#volume');
-    volumeDisplay.textContent = Math.round(video.volume * 100) + "%";
+   var volumeDisplay = document.querySelector('#volume');
+   volumeDisplay.textContent = Math.round(video.volume * 100) + "%";
 }
+
 
 //Styled
 document.querySelector("#vintage").addEventListener("click", function() {
-    if (!video.classList.contains('oldSchool')) {
-        video.classList.add('oldSchool');
-    }
+   if (!video.classList.contains('oldSchool')) {
+       video.classList.add('oldSchool');
+   }
 });
+
 
 //Original
 document.querySelector("#orig").addEventListener("click", function() {
-    if (video.classList.contains('oldSchool')) {
-        video.classList.remove('oldSchool');
-    }
+   if (video.classList.contains('oldSchool')) {
+       video.classList.remove('oldSchool');
+   }
 });
+
 
